68c619a5c307fff81732c7510a32d08985093590
Sean-ButNotHeard <35445013+Sean-ButNotHeard@users.noreply.github.com>
2018-01-18 04:59:15 -0500

    Create header.php
    Basic header.php file which is included (using a PHP include() statement) in each new webpage created.  Title and meta description call on PHP variables defined in the specific pages.

---
+++ InvisibleCreations/includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title><?php echo $metaTitle;?></title>
+        <meta name="description" content="<?php echo $metaDescription;?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+    </head>
+    <body>
+        <div id="container">
+            <div id="logo"><a href="http://invisiblecreationsphotography.com"><img src="images/ICPlogo.png" /></a></div>
+            <div id="nav">
+                <div id="mobileNav"><img src="images/mobileNav.png" /></div>
+                <ul>
+                    <li><a href="http://invisiblecreationsphotography.com/index.php">Home</a></li>
+                    <li><a href="http://invisiblecreationsphotography.com/gallery.php">Gallery</a></li>
+                    <li><a href="http://invisiblecreationsphotography.com/about.php">About Us</a></li>
+                    <li><a href="http://invisiblecreationsphotography.com/contact.php">Contact</a></li>
+                </ul>
+            </div>
