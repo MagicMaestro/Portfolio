ad514df322ae3e6566c19bac88ef52a317028273
Sean-ButNotHeard <35445013+Sean-ButNotHeard@users.noreply.github.com>
2018-01-18 05:02:01 -0500

    Create footer.php
    This basic footer.php file is included (with the use of the PHP include() function) in each webpage.  It utilizes PHP to update the copyright year with the current year.

---
+++ InvisibleCreations/includes/footer.php
@@ -0,0 +1,7 @@
+            <div id="footer">
+                &copy; <?php echo '2011 - '.date("Y").' Sean Reardon'; ?>
+            </div><!-- end of footer -->
+        </div><!-- end of container -->
+        <script src="includes/main.js" type="text/javascript"></script>
+    </body>
+</html>
